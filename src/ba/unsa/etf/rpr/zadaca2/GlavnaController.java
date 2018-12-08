b55cf420b5a797d7e6559f50d497a0b4e8f8c7a7
Amila Sikalo <asikalo2@etf.unsa.ba>
2018-12-08 22:10:20 +0100

    dodavanje opcija u glavna.fxml

--- src/ba/unsa/etf/rpr/zadaca2/GlavnaController.java
+++ src/ba/unsa/etf/rpr/zadaca2/GlavnaController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.zadaca2;
 
+/*Program treba posjedovati glavni prozor sa: glavnim menijem, trakom sa alatima,
+statusnom trakom, a centralni dio prozora treba biti TableView sa spiskom knjiga u
+biblioteci. Forma se treba nalaziti u datoteci glavna.fxml a njen kontroler se treba zvati
+GlavnaController.
+*/
+
+
+
+import javafx.scene.control.Label;
+
 public class GlavnaController {
+    public Label trakaSaAlatimaLabel;
+    public Label glavniMeniLabel;
+    public Label statusnaTrakaLabel;
+    public Label tabelaKnjiga;
+
+
 }
