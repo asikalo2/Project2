1cfe395d3429f2dab9afd5e5cdda0e68891c4f34
Amila Sikalo <asikalo2@etf.unsa.ba>
2018-12-18 15:43:57 +0100

    dodavanje glavnog menija

--- src/ba/unsa/etf/rpr/zadaca2/GlavnaController.java
+++ src/ba/unsa/etf/rpr/zadaca2/GlavnaController.java
@@ -8,7 +8,22 @@ GlavnaController.
 
 
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 
 public class GlavnaController {
     public Label trakaSaAlatimaLabel;
@@ -17,4 +32,100 @@ public class GlavnaController {
     public Label tabelaKnjiga;
 
 
+    public ComboBox FileLabel;
+    public ComboBox EditLabel;
+    public ComboBox HelpLabel;
+    public TextField tbChange;
+    public TextField tbDelete;
+    public TextField tbAdd;
+    public TextField statusMsg;
+
+    /* Opcija Save treba zapisati podatke u XML datoteku, a Open treba učitati iz XML datoteke.
+    Potrebno je koristiti standardni open/save dijalog. Jedini podržani filter za ekstenziju treba biti
+    "XML File" sa ekstenzijom "*.xml". Ove opcije trebaju biti realizirane pomoću metoda doOpen i doSave koje
+    primaju File, a tipa su void. Format datoteke treba biti kao u sljedećem primjeru:
+    <?xml version="1.0"> <biblioteka>    <knjiga brojStranica="500">
+
+      <autor>Meša Selimović</autor>       <naslov>Tvrđava</naslov>       <isbn>abcd</isbn>
+       <datum>20. 3. 1930</datum>    </knjiga>    <knjiga>...</knjiga> </biblioteka>
+       (Svi elementi i atributi su obavezni.) U slučaju da format datoteke nije kao iznad, doOpen metoda treba
+       prikazati dijalog (Alert tipa Error) sa naslovom "Pogrešan format datoteke" i prikladnim tekstom, a
+       postojeći sadržaj biblioteke treba ostati neizmijenjen. */
+
+    public static void doSave(File f) {
+        XMLEncoder encoder=null;
+        try{
+            encoder=new XMLEncoder(new BufferedOutputStream(new FileOutputStream("biblioteka.xml")));
+        }catch(FileNotFoundException fileNotFound){
+            System.out.println("nije mogao kreirati xml datoteku");
+        }
+        encoder.writeObject(f);
+        encoder.close();
+
+    }
+
+    public static void doOpen(File f) {// čitanje iz datoteke
+        File fx = new File();
+        ArrayList<Knjiga> knjige = new ArrayList<Knjiga>();
+        Document xmldoc = null;
+        try {
+            DocumentBuilder docReader
+                    = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            xmldoc = docReader.parse(new File(GlavnaController.class.getResource("biblioteka.xml").getFile()));
+            NodeList nList = xmldoc.getElementsByTagName("biblioteka");
+
+
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node node = nList.item(i);
+                if (node instanceof Element) {
+                    Element e = (Element) node;
+                    String naziv = e.getElementsByTagName("naziv").item(0).getTextContent();
+                    int brojStranica = Integer.parseInt(e.getElementsByTagName("brojStranica").item(0).getTextContent());
+
+                    String stranica = e.getElementsByTagName("brojStranica").item(0).
+                            getAttributes().item(0).getTextContent();
+
+                    Element autor = (Element) e.getElementsByTagName("autor").item(0);
+                    String autorNaziv = autor.getElementsByTagName("autor").item(0).getTextContent();
+                    Element naslov = (Element) e.getElementsByTagName("naslov").item(0);
+                    String naslovNaziv = naslov.getElementsByTagName("naslov").item(0).getTextContent();
+                    Element isbn = (Element) e.getElementsByTagName("isbn").item(0);
+                    String isbnNaziv = isbn.getElementsByTagName("isbn").item(0).getTextContent();
+
+                    Knjiga k = new Knjiga(naslov, autor, isbn, brojStranica);
+                    knjige.add(k);
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("nema datoteke");
+        }
+
+    }
+
+    /*○ Pošto još uvijek nismo obradili rad sa štampačem,
+       opcija Print treba ispisati na standardni izlaz spisak knjiga u sljedećem obliku: Knjige su: Autor, Naslov,
+       ISBN, 123, 01. 01. 1980 Autor, Naslov, ISBN, 123, 01. 01. 1980 ... Ova opcija treba biti implementirana
+       pomoću metode ​dajKnjige​​ u klasi BibliotekaModel koja treba vratiti string istog oblika (bez poruke "Knjige su:"),
+        a zatim metoda ​ispisiKnjige​​ treba pozvati metodu dajKnjige te ispisati "Knjige su:\n" i string koji je vratila
+        spomenuta metoda.*/
+
+
+    /* ○ Meni opcije Add, Change i Delete trebaju obavljati dodavanje, promjenu i brisanje knjige
+        respektivno, što će biti objašnjeno ispod. */
+
+
+    /*○ Opcija About treba prikazati novi prozor sa sljedećim
+        informacijama: naziv programa, naziv autora i neka interesantna sličica (koju ćete prikazati koristeći
+        ImageView kontrolu). ○ Sve opcije menija trebaju imati mnemonike za navigaciju tastaturom, koristeći početna
+         slova, kako je objašnjeno na predavanjima. */
+
+    /*● Traka sa alatima (toolbar) treba sadržavati ikone
+         Add (fx:id ​tbAdd​​), Change (​tbChange​​) i Delete (​tbDelete​​) sa akcijama kao i odgovarajuće opcije menija.*/
+
+    /*Statusna traka treba sadržavati labelu sa fx:id-om ​statusMsg​​ koja će u odgovarajućim situacijama ispisivati poruke:
+        "Program pokrenut.", "Dodajem novu knjigu.", "Knjiga dodana.", "Knjiga nije dodana.", "Mijenjam knjigu.",
+        "Knjiga izmijenjena.", "Knjiga nije izmijenjena.", "Brišem knjigu.", "Knjiga obrisana.", "Knjiga nije obrisana.",
+        "Štampam knjige na standardni izlaz."*/
 }
